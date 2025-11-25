9b7b3a48b89b52dc6d9c015275341f7b04e37077
srutiprusty <sruti.prusty.cse.2022@gmail.com>
2025-11-25 18:42:55 +0530

    imports fixed

--- server/api/index.js
+++ server/api/index.js
@@ -2,13 +2,13 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./utils/db.js"; // Database connection utility
-import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
-import recommendationRoute from "./routes/recommendation.route.js";
-import candidateRoutes from "./routes/candidateRoutes.js"; // Import candidate routes
+import connectDB from "../utils/db.js";
+import userRoute from "./user.route.js";
+import companyRoute from "./company.route.js";
+import jobRoute from "./job.route.js";
+import applicationRoute from "./application.route.js";
+import recommendationRoute from "./recommendation.route.js";
+import candidateRoutes from "./candidateRoutes.js"; // Import candidate routes
 
 import axios from "axios";
 
