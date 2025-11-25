2e7e843b276f5eeda8b2eb5a4fcf9815303d2bea
srutiprusty <sruti.prusty.cse.2022@gmail.com>
2025-11-25 21:22:58 +0530

    vercel and api

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -8,11 +8,22 @@ export const COMPANY_API_END_POINT =
   "https://internnova-server.vercel.app/api/v1/company";
 export const RECOMMENDATION_API_END_POINT =
   "https://internnova-server.vercel.app/api/v1/recommendations"; */
-
+/* 
 export const USER_API_END_POINT = "http://localhost:4000/api/v1/user";
 export const JOB_API_END_POINT = "http://localhost:4000/api/v1/job";
 export const APPLICATION_API_END_POINT =
   "http://localhost:4000/api/v1/application";
 export const COMPANY_API_END_POINT = "http://localhost:4000/api/v1/company";
 export const RECOMMENDATION_API_END_POINT =
-  "http://localhost:4000/api/v1/recommendations";
+  "http://localhost:4000/api/v1/recommendations"; */
+
+export const USER_API_END_POINT =
+  "https://job-portal-internova.vercel.app/api/v1/user";
+export const JOB_API_END_POINT =
+  "https://job-portal-internova.vercel.app/api/v1/job";
+export const APPLICATION_API_END_POINT =
+  "https://job-portal-internova.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT =
+  "https://job-portal-internova.vercel.app/api/v1/company";
+export const RECOMMENDATION_API_END_POINT =
+  "https://job-portal-internova.vercel.app/api/v1/recommendations";
